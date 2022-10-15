a0b37d973fdf45e37e7ea458b8ecb8cffa6c3bb1
lionelritchie29 <lionelrtchieee@gmail.com>
2022-10-15 14:04:16 +0700

    feat: add logout btn

--- frontend/widgets/Layout.tsx
+++ frontend/widgets/Layout.tsx
@@ -19,7 +19,7 @@ const Layout: React.FC<Props> = ({ children, navTitle, withBackBtn = true }) => 
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <nav className='w-full bg-primary text-white p-4 font-medium'>
+      <nav className='w-full bg-primary text-white p-4 font-medium flex justify-between items-center'>
         <div className='flex items-center'>
           {withBackBtn && (
             <button onClick={() => router.back()}>
@@ -28,6 +28,22 @@ const Layout: React.FC<Props> = ({ children, navTitle, withBackBtn = true }) => 
           )}
           <div className='font-bold text-lg mt-1'>{navTitle ?? 'Devcamp Store'}</div>
         </div>
+
+        <button>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='w-6 h-6'>
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9'
+            />
+          </svg>
+        </button>
       </nav>
 
       <main className='p-4'>{children}</main>
