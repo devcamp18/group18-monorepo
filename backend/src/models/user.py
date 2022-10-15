15c7bd586ccfbb7a406acc0798da98cea5b6823c
Widyanto Nugroho <widyantohadi@gmail.com>
2022-10-15 18:00:49 +0700

    add size in product models

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -1,6 +1,6 @@
 import uuid
 from typing import Optional
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, root_validator
 
 
 class User(BaseModel):
@@ -8,8 +8,16 @@ class User(BaseModel):
     name: str = Field(...)
     email: str = Field(...)
     profile_url: str = Field(...)
+    has_body_size: bool = False
     width: Optional[float] = None
     length: Optional[float] = None
 
     class Config:
         allow_population_by_field_name = True
+
+    @root_validator
+    def compute_area(cls, values) -> dict:
+
+        values["has_body_size"] = values["width"] != None and values["length"] != None
+        
+        return values
