c93df5b001af65f670b878fb0f4762e908f80ab8
Widyanto Nugroho <widyantohadi@gmail.com>
2022-10-15 17:48:36 +0700

    add width and lenght as optional

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -8,8 +8,8 @@ class User(BaseModel):
     name: str = Field(...)
     email: str = Field(...)
     profile_url: str = Field(...)
-    width: Optional[float] = Field(...)
-    length: Optional[float] = Field(...)
+    width: Optional[float] = None
+    length: Optional[float] = None
 
     class Config:
         allow_population_by_field_name = True
