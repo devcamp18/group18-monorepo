1b9b5a854682f501f383764640e7bb4b02bcbdae
lionelritchie29 <lionelrtchieee@gmail.com>
2022-10-15 11:28:54 +0700

    feat: add tailwind

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import useWindowSize from '../hooks/useWindowSize';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { width, height } = useWindowSize();
+
+  if (width >= 768) {
+    return (
+      <div className='h-screen w-screen flex items-center justify-center'>
+        This application only supported in mobile or tablet
+      </div>
+    );
+  }
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
