e83a062a0829bf12b337a52dae279ccb166786d0
lionelritchie29 <lionelrtchieee@gmail.com>
2022-10-15 12:22:11 +0700

    feat: add product detail page

--- frontend/components/home/ProductCard.tsx
+++ frontend/components/home/ProductCard.tsx
@@ -1,20 +1,24 @@
+import Link from 'next/link';
+
 /* eslint-disable @next/next/no-img-element */
 const ProductCard = () => {
   return (
-    <div className='min-w-[10rem]'>
-      <div className='border border-gray-200 rounded'>
-        <img
-          className='rounded mx-auto'
-          src='https://i.ibb.co/WxZxqSC/basic-t-shirt-with-logo-man-black-63700-zoom-2.jpg'
-          alt='Shirt'
-          width={150}
-          height={150}
-        />
-      </div>
+    <Link href='/products/123'>
+      <div className='min-w-[10rem]'>
+        <div className='border border-gray-200 rounded'>
+          <img
+            className='rounded mx-auto'
+            src='https://i.ibb.co/WxZxqSC/basic-t-shirt-with-logo-man-black-63700-zoom-2.jpg'
+            alt='Shirt'
+            width={150}
+            height={150}
+          />
+        </div>
 
-      <div className='mt-2'>T-Shirt Polos</div>
-      <div className='font-bold'>Rp. 30.000, 00</div>
-    </div>
+        <div className='mt-2'>T-Shirt Polos</div>
+        <div className='font-bold'>Rp. 30.000, 00</div>
+      </div>
+    </Link>
   );
 };
 
