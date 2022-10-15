6930893531995e77f08f3443967bbfdf9272a883
lionelritchie29 <lionelrtchieee@gmail.com>
2022-10-15 11:39:45 +0700

    feat: add layout

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './widgets/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
+    colors: {
+      primary: '#42B549',
+      'primary-dark': '#01AB5C',
+    },
     extend: {},
   },
   plugins: [],
