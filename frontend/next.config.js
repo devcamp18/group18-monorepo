ae5530c102d6e26c10e0130c1990124049bc5601
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2022-10-15 16:00:15 +0700

    feat: add measure page

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['dimasmiftah.me'],
+    domains: ['dimasmiftah.me', 'user-images.githubusercontent.com'],
   },
 };
 
--- frontend/pages/measure/index.tsx
+++ frontend/pages/measure/index.tsx
@@ -1,54 +1,59 @@
-import Link from 'next/link';
+import Image from 'next/image';
 import Layout from '../../widgets/Layout';
 
-const Settings = () => {
+const Measure = () => {
   return (
     <Layout>
       <form className='flex flex-col gap-4'>
         <div className='flex flex-col gap-2'>
           <label
             className='block text-gray-700 text-sm font-bold '
-            htmlFor='width'
-          >
-            Lebar Baju (Cm)
-          </label>
-          <input
-            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-            id='width'
-            type='number'
-            placeholder='Berapa lebarnya?'
-          />
-        </div>
-        <div className='flex flex-col gap-2'>
-          <label
-            className='block text-gray-700 text-sm font-bold '
             htmlFor='height'
           >
-            Tinggi Baju (Cm)
+            Tinggi Badan (Cm)
           </label>
           <input
             className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
             id='height'
             type='number'
-            placeholder='Berapa tingginya?'
+            placeholder='Gausah dilebihin'
           />
         </div>
-        <p>
-          Gak tau ukuran bajumu? Pap badan kamu dan kami bakal bantu ukurin.{' '}
-          <Link href='/measure'>
-            <a className='text-primary font-semibold'>Ukur sekarang!</a>
-          </Link>
-        </p>
+        <div className='flex flex-col gap-2'>
+          <label
+            className='block text-gray-700 text-sm font-bold '
+            htmlFor='picture'
+          >
+            Pap dong kakak
+          </label>
+          <div className='flex items-center'>
+            <Image
+              alt='Contoh pose yang benar'
+              src='https://user-images.githubusercontent.com/32232332/195977087-5e058a05-dca7-4d0d-89bb-17a8c7feff03.png'
+              width={200}
+              height={220}
+            />
+            <span className='text-gray-600 text-sm'>Contoh pose</span>
+          </div>
+          <input
+            type='file'
+            accept='image/*'
+            capture='user'
+            multiple={false}
+            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+            id='picture'
+          />
+        </div>
 
         <button
           className='bg-primary-dark text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full'
           type='button'
         >
-          Simpan
+          Prediksi Ukuran Baju
         </button>
       </form>
     </Layout>
   );
 };
 
-export default Settings;
+export default Measure;
