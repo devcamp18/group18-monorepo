346d9c6c7e99f632f4fbd5ac2b56fb6b34a1a84b
Widyanto Nugroho <widyantohadi@gmail.com>
2022-10-15 23:23:16 +0700

    kelar gengs

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@ import uvicorn
 import sys
 
 
+sys.path.insert(0, './yolov7')
+
 if __name__ == '__main__':
     args = sys.argv
     if args[1] == "run":
--- backend/src/services/predictor.py
+++ backend/src/services/predictor.py
@@ -11,18 +11,28 @@ class PredictorService:
     @inject
     def __init__(
         self,
-        # body_predictor: BodySizePredictor
+        body_predictor: BodySizePredictor
     ) -> None:
-        # self.body_predictor = body_predictor
-        pass
+        self.body_predictor = body_predictor
 
     def predict_body(self, spec: GetBodySizePredictorRequest) -> BodySizeResponse:
-        # width, length = self.body_predictor.calculate(
-        #     person_height=spec.height,
-        #     image_bytes=spec.image_string,
-        # )
+        width, length = self.body_predictor.calculate(
+            person_height=spec.height,
+            image_path="test_data/example_1.jpg",
+        )
 
         return BodySizeResponse(
-            width=1,
-            length=1,
+            width=width,
+            length=length,
+        )
+
+    def predict_body_file(self, height: float, file: bytes) -> BodySizeResponse:
+        width, length = self.body_predictor.calculate(
+            person_height=height,
+            image_bytes=file,
+        )
+
+        return BodySizeResponse(
+            width=width,
+            length=length,
         )
