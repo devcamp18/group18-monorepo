e4de4fbb85bd54162300328f99c5c5461b53abd5
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2022-10-15 16:01:06 +0700

    feat: change navbar color

--- frontend/widgets/Layout.tsx
+++ frontend/widgets/Layout.tsx
@@ -1,11 +1,11 @@
-import Head from 'next/head';
 import {
+  ArrowRightOnRectangleIcon,
   ChevronLeftIcon,
   UserCircleIcon,
-  ArrowRightOnRectangleIcon,
 } from '@heroicons/react/20/solid';
-import { useRouter } from 'next/router';
+import Head from 'next/head';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { useAuth } from '../hooks/useAuth';
 
 type Props = {
@@ -14,7 +14,11 @@ type Props = {
   withBackBtn?: boolean;
 };
 
-const Layout: React.FC<Props> = ({ children, navTitle, withBackBtn = true }) => {
+const Layout: React.FC<Props> = ({
+  children,
+  navTitle,
+  withBackBtn = true,
+}) => {
   const router = useRouter();
   const { currentUser, signOut } = useAuth();
 
@@ -26,14 +30,16 @@ const Layout: React.FC<Props> = ({ children, navTitle, withBackBtn = true }) => 
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <nav className='w-full bg-primary text-white p-4 font-medium flex justify-between items-center'>
+      <nav className='w-full bg-primary-dark text-white p-4 font-medium flex justify-between items-center'>
         <div className='flex items-center'>
           {withBackBtn && (
             <button onClick={() => router.back()}>
               <ChevronLeftIcon className='w-8 h-8 mr-2' />
             </button>
           )}
-          <div className='font-bold text-lg mt-1'>{navTitle ?? 'Devcamp Store'}</div>
+          <div className='font-bold text-lg mt-1'>
+            {navTitle ?? 'Devcamp Store'}
+          </div>
         </div>
 
         {!router.pathname.includes('/auth/login') && (
